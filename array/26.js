893799e9f3faaa70ff0360caa0edc4b7a179de0d
sajith <triangle4business@gmail.com>
2024-10-20 11:59:32 +0530

    first commit

---
+++ array/26.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    let count =0
+    for(let i=0;i<nums.length-1;i++){
+        if(nums[i]!=nums[i+1]){
+            nums[count]= nums[i+1]
+            count++
+        }else{
+            countinew
+        }
+    }
+    
+};
+
+removeDuplicates
